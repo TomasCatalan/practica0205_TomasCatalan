ae9dfdf1f02072421c19f097dd01dbb07910cd61
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-11-14 13:35:52 +0100

    Create Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,12 @@
+def x (y):
+    '''Funcion que muestra por pantalla un nombre
+    
+    Parametros: 
+       - y : nombre introducido en la cadena
+       
+    Salida: Saludo con el nombre'''
+    
+    print("¡hola ", y, "!")
+
+z = input("introduzca un nombre")
+x(z) 
